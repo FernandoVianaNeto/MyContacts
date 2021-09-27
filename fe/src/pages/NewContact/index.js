ed5f281c3cf3f9f37326750c326a0bc26288f5a4
Fernando <fernando.viana.nt@gmail.com>
2021-09-27 10:48:17 -0300

    components input e select

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,3 +1,18 @@
+import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
 export default function NewContact() {
-  return 'new contact';
+  return (
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+
+      <Input placeholder="Nome" />
+      <Select>
+        <option>Instagram</option>
+      </Select>
+    </>
+  );
 }
